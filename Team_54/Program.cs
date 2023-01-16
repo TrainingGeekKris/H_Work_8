1218d365f881d3bd21ca2f764a063e5ba646b0b4
Kristina <petrovnina22@mail.ru>
2023-01-16 20:36:17 +0300

    commit_54

---
+++ Team_54/Program.cs
@@ -0,0 +1,65 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] GetArray()
+{
+    int [,] array = new int [rows,columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i,j] = new Random().Next(10);
+        }
+    }
+    return array;
+}
+int [,] ChageColumns(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int z = 0; z < array.GetLength(1)-1; z++)
+            {
+                if (array[i,z]<array[i,z+1])
+                {
+                    int temp = array[i,z+1];
+                    array[i,z+1] = array[i,z];
+                    array[i,z] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] matrix = GetArray();
+PrintArray(matrix);
+
+Console.WriteLine();
+
+PrintArray(ChageColumns(matrix));
